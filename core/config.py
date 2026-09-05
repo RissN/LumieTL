1f8c7bac49f2acfb1f4ad1f3716595fabafa4406
Azizi Faris <farisazizishie@gmail.com>
2026-09-05 21:16:28 +0700

    chore: remove assets folder and icon.ico

--- core/config.py
+++ core/config.py
@@ -5,7 +5,6 @@ import sys
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-ASSETS_DIR = BASE_DIR / "assets"
 
 # User persistent data directory
 if sys.platform == "win32":
